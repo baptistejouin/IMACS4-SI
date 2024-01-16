8e2116ff389e9dc312782d0d470e781774f2ef8a
baptistejouin <baptiste.jouin44@gmail.com>
2024-01-16 10:37:18 +0100

    TP3 (wip)

--- TP3_exo1_triangle_blanc/main.cpp
+++ TP3_exo1_triangle_blanc/main.cpp
@@ -1,4 +1,9 @@
+#include "glimac/default_shader.hpp"
 #include "p6/p6.h"
+
+/**
+ * @docs: https://julesfouchy.github.io/Learn--OpenGL/TP3/dessiner-un-triangle-blanc#le-vbo
+ */
 
 int main()
 {
@@ -9,13 +14,74 @@ int main()
      * HERE SHOULD COME THE INITIALIZATION CODE
      *********************************/
 
+    // Create a VAO
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    // Bind the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    // Create an array of vertices to draw a triangle
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, // Left
+        0.5f, -0.5f,  // Right
+        0.0f, 0.5f,   // Top
+    };
+
+    // Send the vertices to the GPU
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+
+    // Unbind the VBO to avoid modification
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Create a VAO
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    // Bind the VAO
+    glBindVertexArray(vao);
+
+    // Bind the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    // vertex attribute activation
+    static constexpr GLuint VERTEX_ATTR_POSITION = 0;
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+
+    // Unbind the VBO to avoid modification
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbind the VAO to avoid modification
+    glBindVertexArray(0);
+
     // Declare your infinite update loop.
     ctx.update = [&]() {
         /*********************************
          * HERE SHOULD COME THE RENDERING CODE
          *********************************/
+
+        // Clear the color buffer to avoid accumulation (here we only need to clear the color buffer)
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Bind the VAO
+        glBindVertexArray(vao);
+
+        // Use the default shader provided by glimac
+        glimac::bind_default_shader();
+
+        // Draw the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Unbind the VAO
+        glBindVertexArray(0);
     };
 
     // Should be done last. It starts the infinite loop.
     ctx.start();
+
+    // Free the memory allocated for the VBO
+    glDeleteBuffers(1, &vbo);
+
+    // Free the memory allocated for the VAO
+    glDeleteVertexArrays(1, &vao);
 }
